127c5806de551c64b426f30d5e3c32cffaecb74e
Renne Castro <rcastro2@schools.nyc.gov>
2019-04-17 03:06:48 -0400

    reworks Database and RouteHandler classes

--- BasicHttpServerExplore.java
+++ BasicHttpServerExplore.java
@@ -3,18 +3,13 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.InetSocketAddress;
-import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
 
+import java.net.InetSocketAddress;
+import java.util.Map;
 import java.sql.*;
 //javac -cp sqlite-jdbc-3.23.1.jar; BasicHttpServerExample.java
 
-public class BasicHttpServerExample {
+public class BasicHttpServerExplore {
 
     public static void main(String[] args) throws IOException {
         int port = (args.length == 0)?8500:Integer.parseInt(args[0]);
@@ -29,8 +24,8 @@ public class BasicHttpServerExample {
         String template = Input.readFile("test.html");
         server.createContext("/test", new RouteHandler(template));
 
-        template = Input.readFile("sqltest.html");
-        server.createContext("/sqltest", new RouteHandler(template));
+        template = Input.readFile("retrieveCustomers.html");
+        server.createContext("/retrieveCustomers", new RouteHandler(template));
 
         //Routes for static data from database
         //db.runQuery("INSERT INTO customers(CustomerId,FirstName,Lastname,Email) Values (1234,'Renne','Castro','r@aol.com')");
@@ -40,44 +35,37 @@ public class BasicHttpServerExample {
         data = db.selectData("SELECT ArtistId, Name FROM artists");
         server.createContext("/artists", new RouteHandler(data));
 
+        data = db.selectData("SELECT invoices.CustomerId, invoices.InvoiceId FROM customers INNER JOIN invoices on customers.CustomerId = invoices.CustomerId;");
+        server.createContext("/invoices", new RouteHandler(data));
+
         //Routes for live data from database
         server.createContext("/livecustomers", new RouteHandler(db,"SELECT CustomerId, LastName FROM customers"));
 
         //More advance examples
-        server.createContext("/customers2", new HttpHandler(){
+        server.createContext("/livecustomers2", new HttpHandler(){
             public void handle(HttpExchange exchange) throws IOException {
-                String response = db.selectData("SELECT CustomerId, LastName FROM customers");
-                exchange.sendResponseHeaders(200, response.length());
-                OutputStream os = exchange.getResponseBody();
-                os.write(response.getBytes());
-                os.close();
+                String response = db.selectData("SELECT CustomerId, FirstName, LastName FROM customers");
+                RouteHandler.send(response,exchange);
             }
         });
         server.createContext("/postTest", new HttpHandler(){
             public void handle(HttpExchange exchange) throws IOException {
-                    // parse request
-                    Map<String, Object> parameters = new HashMap<String, Object>();
-                    InputStreamReader isr = new InputStreamReader(exchange.getRequestBody(), "utf-8");
-                    BufferedReader br = new BufferedReader(isr);
-                    String response = br.readLine();
-                    System.out.println("Info: " + response);
-                    exchange.sendResponseHeaders(200, response.length());
-                    OutputStream os = exchange.getResponseBody();
-                    os.write(response.toString().getBytes());
-                    os.close();
+					Map<String, Object> parameters = RouteHandler.parseParameters("post",exchange);
+					
+                    System.out.println("Info: " + parameters.get("id"));
+					String response = "Info: " + parameters.get("id");
+
+                    RouteHandler.send(response,exchange);
             }
         });
         server.createContext("/getTest", new HttpHandler(){
             public void handle(HttpExchange exchange) throws IOException {
-                    // parse request
-                    Map<String, Object> parameters = new HashMap<String, Object>();
-                    URI requestedUri = exchange.getRequestURI();
-                    String response = requestedUri.getRawQuery();
-                    System.out.println("Info: " + response);
-                    exchange.sendResponseHeaders(200, response.length());
-                    OutputStream os = exchange.getResponseBody();
-                    os.write(response.toString().getBytes());
-                    os.close();
+					Map<String, Object> parameters = RouteHandler.parseParameters("get",exchange);
+					
+                    System.out.println("Info: " + parameters.get("data"));
+					String response = "Info: " + parameters.get("data");
+					
+                    RouteHandler.send(response,exchange);
             }
         });
 
@@ -85,6 +73,7 @@ public class BasicHttpServerExample {
         server.start(); 
         System.out.println("Server is listening on port " + port );
     }    
+
 }
 /*
 Resources
--- Database.java
+++ Database.java
@@ -31,27 +31,12 @@ public class Database{
         return success; 
     }
     public String selectData(String sql){
-        String result = "";
+        String result = null;
         connect();
         //System.out.println(sql);
-        try (Statement stmt  = conn.createStatement();
-            ResultSet rs    = stmt.executeQuery(sql)){
-            
-            //Get field names            
-            ResultSetMetaData metadata = rs.getMetaData();
-            int columnCount = metadata.getColumnCount();   
-
-            String field, build = "[";
-            // loop through the result set
-            while (rs.next()) {
-                build += "{";
-                for (int i = 1; i <= columnCount; i++) {
-                    field = metadata.getColumnName(i);
-                    build += "\"" + field + "\":\"" + rs.getString(field) + "\",";
-                }
-                build = build.substring(0,build.length()-1) + "},";
-            }
-            result = build.substring(0,build.length()-1) + "]";
+        try (Statement stmt  = conn.createStatement()){
+            ResultSet rs    = stmt.executeQuery(sql);
+            result = jsonify(rs);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         } finally {
@@ -70,7 +55,30 @@ public class Database{
             success = false;
         } finally {     
             close();
-            return success;
-        }
+            
+        }return success;
     }
+    public static String jsonify(ResultSet rs){
+		String result = "";
+        try{       
+            //Get field names            
+            ResultSetMetaData metadata = rs.getMetaData();
+            int columnCount = metadata.getColumnCount();   
+
+            String field, build = "[";
+            // loop through the result set
+            while (rs.next()) {
+                build += "{";
+                for (int i = 1; i <= columnCount; i++) {
+                    field = metadata.getColumnName(i);
+                    build += "\"" + field + "\":\"" + rs.getString(field) + "\",";
+                }
+                build = build.substring(0,build.length()-1) + "},";
+            }
+            result = build.substring(0,build.length()-1) + "]";
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return result;
+	}
 }
